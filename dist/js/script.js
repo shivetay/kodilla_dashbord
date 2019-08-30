22861253d8299777b1b069e8ddda81f241abe72f
Łukasz Dawidowicz <lddev@outlook.com>
2019-08-30 20:33:24 +0200

    add phon menu and hamburger

---
+++ dist/js/script.js
@@ -0,0 +1,16 @@
+{
+	'user strict'
+
+	const inputCheck = document.querySelector('.navigation__hamburger--toggler');
+
+	inputCheck.addEventListener('change', function(e){
+		e.preventDefault();
+
+		if (inputCheck.checked === true ){
+			document.querySelector('.navigation__item ul').classList.add('visible');
+		} else {
+			console.log('uncheck');
+			document.querySelector('.navigation__item ul').classList.remove('visible');
+		}
+	});
+}
